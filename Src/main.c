77b23903b2c5f2c6878ff36381b33e553079425e
John Whittington <git@jbrengineering.co.uk>
2020-07-14 15:43:51 +0200

    clock config for internal osc

--- Src/main.c
+++ Src/main.c
@@ -4,6 +4,9 @@
 #include "pcan_protocol.h"
 #include "pcan_usb.h"
 
+// remove to use 48 MHz internal clock
+/* #define EXTERNAL_CLOCK 16 */
+
 void HAL_MspInit( void )
 {
   __HAL_RCC_SYSCFG_CLK_ENABLE();
@@ -14,6 +17,7 @@ void HAL_MspInit( void )
   __HAL_RCC_GPIOB_CLK_ENABLE();
 }
 
+#ifdef EXTERNAL_CLOCK
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -27,9 +31,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
 
   /* HSE = 16MHZ */
+#if EXTERNAL_CLOCK == 16
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL3;
   /* HSE = 8MHZ */
-  //RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+#else
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+#endif
   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
 
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
@@ -47,6 +54,31 @@ void SystemClock_Config(void)
 
   HAL_RCCEx_PeriphCLKConfig( &PeriphClkInit );
 }
+#else
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+    |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+}
+#endif
 
 void SysTick_Handler( void )
 {
@@ -58,7 +90,7 @@ int main( void )
   HAL_Init();
 
   SystemClock_Config();
-  
+
   pcan_usb_init();
   pcan_led_init();
   pcan_timestamp_init();
